48694ddcff1a76bdea875f2bb72eb856e2bffcee
txzzz <toxaz0@gmail.com>
2022-12-04 10:08:45 +0100

    2022 day 4

---
+++ adventofcode2022/day4b.php
@@ -0,0 +1,19 @@
+<?php
+
+function between($x, $min, $max) {
+	return ($x >= $min && $x <= $max);
+}
+
+$pairs = explode("\n", trim(file_get_contents('data/day4.txt')));
+
+$contains = 0;
+while(count($pairs)) {
+	$pair = array_shift($pairs);
+	list($a, $b) = explode(',', $pair);
+	list($aMin, $aMax) = explode('-', $a);
+	list($bMin, $bMax) = explode('-', $b);
+	if((between($aMin, $bMin, $bMax) || between($aMax, $bMin, $bMax)) || (between($bMin, $aMin, $aMax) || between($bMax, $aMin, $aMax))) {
+		$contains++;
+	}
+}
+print "Fully contains: $contains\n";
